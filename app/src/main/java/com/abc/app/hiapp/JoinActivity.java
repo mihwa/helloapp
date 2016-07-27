3a4284a8cd2ca24df52879311aedbad8e560f557
mihwa <mycnex@gamil.com>
2016-07-27 17:50:31 +0900

    0727

--- app/src/main/java/com/abc/app/hiapp/JoinActivity.java
+++ app/src/main/java/com/abc/app/hiapp/JoinActivity.java
@@ -7,36 +7,39 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
-public class MainActivity extends Activity implements View.OnClickListener {
-    EditText et_id, et_pw;
-    Button bt_login, bt_join;
+public class JoinActivity extends Activity implements View.OnClickListener {
+
+    EditText et_id, et_pw, et_name, et_ssn, et_email, et_phone;
+    Button bt_join, bt_reset;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_join);
         et_id = (EditText) findViewById(R.id.et_id);
         et_pw = (EditText) findViewById(R.id.et_pw);
-        bt_login = (Button) findViewById(R.id.bt_login);
+        et_name = (EditText) findViewById(R.id.et_name);
+        et_ssn = (EditText) findViewById(R.id.et_ssn);
+        et_email = (EditText) findViewById(R.id.et_email);
+        et_phone = (EditText) findViewById(R.id.et_phone);
+
         bt_join = (Button) findViewById(R.id.bt_join);
-        bt_login.setOnClickListener(this);
+        bt_reset = (Button) findViewById(R.id.bt_reset);
+
         bt_join.setOnClickListener(this);
+        bt_reset.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.bt_join:
-                startActivity(new Intent(this, JoinActivity.class));
-
                 break;
-            case R.id.bt_login:
-                Toast.makeText(MainActivity.this,
-                        "ID:" + et_id.getText().toString()
-                                + "PW:" + et_pw.getText().toString(), Toast.LENGTH_LONG).show();
-
+            case R.id.bt_reset:
+                startActivity(new Intent(JoinActivity.this, MainActivity.class));
                 break;
         }
     }
